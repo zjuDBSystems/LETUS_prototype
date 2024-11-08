3d43503584e41bc685fb18e04498025936957aa9
xinyu.chen <xinyu.chen@zju.com>
2024-11-08 16:47:47 +0800

    [feat] implement trace_replay
    trace_replay read a workload trace using jsonCPP.

--- workload/exes/trace_replay.cc
+++ workload/exes/trace_replay.cc
@@ -1,3 +1,27 @@
 #include <iostream>
-#include <fstrem>
+#include <fstream>
+#include <string>
 
+#include <json/json.h>
+
+using namespace std;
+
+
+int main(int argc, char **argv) {
+    string line;
+    ifstream trace("/home/xinyu.chen/LETUS_prototype/workload/traces/log");
+    if (!trace) {
+        std::cerr << "无法打开文件" << std::endl;
+        return 1;
+    } 
+    while (std::getline(trace, line)) { // 逐行读取文件内容
+        // std::cout << line << std::endl;
+        Json::Reader reader;
+	    Json::Value value;
+        if(reader.parse(line, value))
+        {
+            cout << value["op"].asString() << endl;
+        }
+    }
+    return 0;
+}
