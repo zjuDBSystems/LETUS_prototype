3490bd4bc39e2cc22cff5e1a4abd56e91570ef79
xinyu.chen <xinyu.chen@zju.com>
2024-12-11 11:00:13 +0800

    [merge] xinyu.chen <- dev

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ rm -rf data/*
 # 设置更大的堆栈大小以获取更好的调试信息
 # ulimit -n 4096
 # 添加 ASAN 选项以输出更详细的日志
-export ASAN_OPTIONS=detect_leaks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1:detect_invalid_pointer_pairs=2
+# export ASAN_OPTIONS=detect_leaks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1:detect_invalid_pointer_pairs=2
 # 将输出重定向到文件
-./build/bin/get_put_2 -n 3 2>asan_log.txt
+./build/bin/get_put_2 -n 20 2>asan.log
