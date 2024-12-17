810345aaf7406aa971d1b9d4c1885a452a6e483f
self-consstency <dezhi2002@outlook.com>
2024-12-17 17:43:55 +0800

    [merge] change README.md
    * [fix] remove aux_source_directory in CMakeLists.txt
    
    * [fix] add simple in CMakeLists.txt
    
    * [fix] fix key name format
    
    * [fix] make cmake debug-able
    
    * [fix] add OpenSSL to cmake
    
    * [merge] merge dev -> xinyu.chen
    
    * [feat] add go interface
    
    add main.go
    
    * [release] run 500 gets and puts
    
    * [release] run 500 gets and puts
    
    key length = 5
    value length = 256
    
    * [release] run 5000 gets and puts
    
    key length = 5
    value length = 256
    
    * [feat] add go interface
    
    error: undefined reference to
    
    * [feat] add go interface
    
    successfully run Get and Put
    
    * [feat] add go interface
    
    successfully run Get and Put
    
    * [feat] add go interface
    
    successfully test multiple go files
    
    * [feat] add go interface
    
    successfully test get and put
    
    * [doc] add db-interface.md
    
    * [feat] add go interface
    
    [incomplete] implement the NewBatch interface
    
    * [feat] add go interface
    
    implement NewBatch interface
    
    * [feat] add experiment result ploting
    
    * [test] add result plotting for get_put
    
    * [test] add result plotting for get_put
    
    * [fix] each kv is assigned a unique version
    
    * [fix] each kv is assigned a unique version
    
    * [fix] clear data dir for each test
    
    * [feat] add get_put_2 to support historical queries
    
    * [feat] add commit interface in gowrapper
    
    * [fix] some sanitize
    
    * [test] test get_put
    
    * [merge] xinyu.chen <- dev
    
    * [doc] add the notice of changing data_path
    
    * [test] test result plotting
    
    * [test] draw result plot
    
    * [test] add wrong count as a metric
    
    * [fix] add directories into run.sh
    
    * [release] run get_put correctly.
    
    * [doc] complete the README.md
    
    add description of LETUS
    add instruction of using LETUS_prototype
    add description of workloads
    
    * [doc] complete the README.md
    
    add description of LETUS
    add instruction of using LETUS_prototype
    add description of workloads
    
    * [doc] complete the README.md
    
    add description of LETUS
    add instruction of using LETUS_prototype
    add description of workloads
    
    ---------
    
    Co-authored-by: xinyu.chen <xinyu.chen@zju.com>

--- exps/run.sh
+++
@@ -1,5 +0,0 @@
-export PATH=/usr/bin:/usr/include:/usr/local/include:/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/local/sbin:/usr/local/lib:/usr/local/bin:/usr/sbin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin;
-export LD_LIBRARY_PATH=/usr/include:/usr/local/lib:/usr/local/include:/usr/lib/x86_64-linux-gnu;
-
-cd /home/xinyu.chen/LETUS_prototype/build/bin
-./trace_replay
